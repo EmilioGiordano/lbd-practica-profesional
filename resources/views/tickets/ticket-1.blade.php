d0be377e015cd7fa274f5292180c158808f15266
EmilioGiordano <giordanoemilio21@gmail.com>
2026-05-24 21:58:27 -0300

    Update branding and ticket views

--- resources/views/tickets/ticket-1.blade.php
+++ resources/views/tickets/ticket-1.blade.php
@@ -1,10 +1,4 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h1 class="text-xl font-semibold leading-tight text-zinc-50">
-            Ticket 1
-        </h1>
-    </x-slot>
-
     <div class="py-8">
         <div class="mx-auto w-full max-w-[1800px] px-4 sm:px-6 lg:px-10">
             <x-ticket-playground :ticket-number="1" title="Ticket 1" />
